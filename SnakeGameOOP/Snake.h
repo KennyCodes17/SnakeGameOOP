b830e2d7bf8b8f16b229e98f9b7a4f55df95ba7a
KennyCodes17 <kennycrc7@gmail.com>
2024-10-17 16:28:40 -0700

    game board drawn

---
+++ SnakeGameOOP/Snake.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "raylib.h"
+
+//colors
+Color green = {173, 204, 96, 255};
+Color darkGreen = {43, 51, 24, 255};
+
+#include "vector"
+
+class Snake
+{
+public:
+	void Draw();
+
+private:
+
+};
--- SnakeGameOOP/SnakeGameOOP.cpp
+++ SnakeGameOOP/SnakeGameOOP.cpp
@@ -1,6 +1,25 @@
+#include "Snake.h"
+#include "GameLogic.cpp" 
 #include <iostream>
+#include <conio.h> 
+#include "raylib.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    InitWindow(750, 750, " Snake Game");
+    SetTargetFPS(60);
+
+    while (WindowShouldClose() == false)
+    {
+        BeginDrawing();
+
+        ClearBackground(green);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 }
